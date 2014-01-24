6d13e5df205a039f83cbd21253e50fb8dc2dfb22
Evan Brown <noggin@gmail.com>
2014-01-24 15:09:21 -0600

    Refactor commit

--- include/UserAuth.js
+++ include/UserAuth.js
@@ -1,55 +1,43 @@
 var couchbase = require('couchbase');
 var sys = require('sys');
-var events = require('events');
 
-function Login() {
-    if (false === (this instanceof Login)) {
-        return new Login();
-    }
-    
-    events.EventEmitter.call(this);
-}
-sys.inherits(Login, events.EventEmitter);
+var UserAuth = {
+    motd: function(){
+        return "Welcome to my crazy game! Please log in!\n\nName:";
+    },
 
-Login.prototype.motd = function(){
-    return "Welcome to my crazy game! Please log in!\n\nName:";
-};
-
-Login.prototype.verify_name = function(name){
-    var self = this;
-    
-    var db = new couchbase.Connection({
-        host: ['10.4.2.133',
-               '10.4.2.126',
-               '10.4.2.128'],
-        bucket: 'gamesim',
-        password: 'password1'
-    }, function(err){
-        if (err) {
-            console.log('DB Connection Error', err);
-        } else {
-            console.log('DB Connected');
-        }
-    });
-    
-    db.view('dev_by_name', 'By_Name').query(function(err, results){
-        for (var i = 0; i< results.length; i++){
-            var found = false;
-            if(results[i].value == name){
-                found = true;
+    verify_name: function(clients, name, socket){
+        var db = new couchbase.Connection({
+            host: ['127.0.0.1'],
+            bucket: 'tbdmud',
+            password: 'password1'
+        }, function(err){
+            if (err) {
+                console.log('DB Connection Error', err);
+            } else {
+                console.log('DB Connected');
             }
-        }
-        if (found) {
-            self.emit('userValid');
-        }
-        if (!found){
-            self.emit('userInvalid');
-        }
-    });
+        });
+        
+        db.view('dev_players', 'By_Name').query(function(err, results){
+            for (var i = 0; i< results.length; i++){
+                var found = false;
+                if(results[i].value == name){
+                    found = true;
+                }
+            }
+            if (found) {
+                socket.emit('userValid', {clients: clients, socket: socket});
+            }
+            if (!found){
+                socket.emit('userInvalid', socket);
+            }
+        });
+    },
+
+    process: function (name, password){
+        return 'processing';
+    }
 };
 
-Login.prototype.process = function (name, password){
-    return 'processing';
-};
-
-exports.Login = Login;
+exports.UserAuth = UserAuth;
